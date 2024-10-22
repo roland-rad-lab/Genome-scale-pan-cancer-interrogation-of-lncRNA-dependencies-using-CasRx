4873c2706d8920bff777c40fea5e96c940d99fbb
riccardo-trozzo <riccardo.trozzo21@gmail.com>
2024-10-22 17:35:24 +0200

    cleaned code

--- gRNA_design/modules/off_targets_filtering.sh
+++ gRNA_design/modules/off_targets_filtering.sh
@@ -14,5 +14,4 @@ new_file="${filtered_prediction_dir}/${old_file%.csv}_filtered.csv"
 echo "filtering ${old_file} to ${new_file}"
 head -n 1 ${prediction_file} > ${new_file}
 tail +2 ${prediction_file} | awk -F "\"*,\"*" '{print">"$1,$2}' | tr " " "\n" | bowtie2 --end-to-end -D 5 -R 1 -N 0 -L 23 -i S,0,2.50 --nofw -f -p 75 -x ${off_targets_index} -U - | samtools view -q 42 | awk '{print$1}' | grep -f - ${prediction_file} >> ${new_file}
-# awk -F "\"*,\"*" '{print">"$1,$2}' ${prediction_file} | tr " " "\n" | bowtie2 --end-to-end -D 5 -R 1 -N 0 -L 23 -i S,0,2.50 --nofw -f -p 75 -x ${off_targets_index} -U - | samtools view -q 42 | awk '{print$1}' | grep -f - ${prediction_file} >> ${new_file}
 done
